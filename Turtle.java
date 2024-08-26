eb7566faf849648baac9f8aab32e91f68b425548
Aparna526 <aparnakondapaturu@gmail.com>
2024-08-26 19:49:07 +0530

    one

--- Turtle.java
+++ Turtle.java
@@ -1,4 +1,20 @@
-public class Turtle{
+public class Turtle implements Animals{
+
+  @Override
+  public String Eat(String food) {
+    // TODO Auto-generated method stub
+    return food;
+  }
 // implements Animal Interface
   // Provide functionality for eat()
+
+  public static void main(String[] args) {
+    Turtle t = new Turtle();
+    System.out.println(t.Eat("Grass"));
+
+  }
+
+  
+
+
 }
