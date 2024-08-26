76708fec16fa3500b39e5aecda4484f366fcbde5
revathivinnakota90@gmail.com <revathivinnakota90@gmail.com>
2024-08-26 20:44:54 +0530

    Revathi

--- Emp.java
+++ Emp.java
@@ -1,5 +1,32 @@
 public class Emp{
-// Take 4 Variables of Emp Data With an Access Modifier Private
-  // Prepare toString()
-  
+  private int eid;
+  private String ename;
+  private double sal;
+  private char gen;
+
+  public Emp(int eid, String ename, double sal, char gen) {
+    this.eid = eid;
+    this.ename = ename;
+    this.sal = sal;
+    this.gen = gen;
+  }
+  public int getEid() {
+    return eid;
+  }
+
+  public String getEname() {
+    return ename;
+  }
+
+  public double getSal() {
+    return sal;
+  }
+
+  public char getGen() {
+    return gen;
+  }
+@Override
+  public String toString() {
+    return "Emp [eid=" + eid + ", ename=" + ename + ", sal=" + sal + ", gen=" + gen + "]";
+  }
 }
--- Turtle.java
+++ Turtle.java
@@ -1,4 +1,10 @@
-public class Turtle{
-// implements Animal Interface
-  // Provide functionality for eat()
+public class Turtle implements Animals{
+  @Override
+  public void eat(String food) {
+    System.out.println("Animals Eating : "+food);
+  }
+  public static void main(String[] args) {
+    Turtle t=new Turtle();
+    t.eat("Grass");
+  };
 }
