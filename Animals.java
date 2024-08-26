10390915e6da9eaf335958c5f02ad5933c0a8ab9
Rohit08012004 <rohitsthakre@gmail.com>
2024-08-26 19:08:15 +0530

    done

--- Animals.java
+++ Animals.java
@@ -1,4 +1,8 @@
 public interface Animals{
+
+
+    public void eat(string name,int age,char gender);
+
 // Prepare a Functoin called Eat() with String Paramater
   
 }
--- Turtle.java
+++ Turtle.java
@@ -1,4 +1,34 @@
-public class Turtle{
+public class Turtle implements Animals{
+
+  @Override
+  public void eat(string name, int age, char gender) {
+    
+
+
+ System.out.println(name);
+    
+  }
+
+
+
+
+  public static void main(String[] args) {
+   Turtle out=new Turtle();
+  //  out.eat("rohit", 9, "m");
+  out.eat ("rohit", 12, "m");
+
+
+  }
 // implements Animal Interface
   // Provide functionality for eat()
-}
+
+
+
+
+  
+
+
+
+
+ 
+  }
