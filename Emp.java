ae747f9ad0f4670424665b3c4ff0c2feba565fba
vijay767492 <vijaydandu59@gmail.com>
2024-08-26 20:15:10 +0530

    vijay dandu/oops encapsulation

--- Emp.java
+++ Emp.java
@@ -1,5 +1,61 @@
-public class Emp{
-// Take 4 Variables of Emp Data With an Access Modifier Private
-  // Prepare toString()
-  
+public class Emp {
+  // Private fields for encapsulation
+  private int id;
+  private String name;
+  private double salary;
+  private long phone;
+
+  // Constructor with all parameters
+  public Emp(int id, String name, double salary, long phone) {
+      this.id = id;
+      this.name = name;
+      this.salary = salary;
+      this.phone = phone;
+  }
+
+  // Default constructor
+  public Emp() {
+  }
+
+  // Getter and Setter methods for field 'id'
+  public int getId() {
+      return id;
+  }
+
+  public void setId(int id) {
+      this.id = id;
+  }
+
+  // Getter and Setter methods for field 'name'
+  public String getName() {
+      return name;
+  }
+
+  public void setName(String name) {
+      this.name = name;
+  }
+
+  // Getter and Setter methods for field 'salary'
+  public double getSalary() {
+      return salary;
+  }
+
+  public void setSalary(double salary) {
+      this.salary = salary;
+  }
+
+  // Getter and Setter methods for field 'phone'
+  public long getPhone() {
+      return phone;
+  }
+
+  public void setPhone(long phone) {
+      this.phone = phone;
+  }
+
+  // Overriding toString() method to provide a string representation of the object
+  @Override
+  public String toString() {
+      return "Emp [id=" + id + ", name=" + name + ", salary=" + salary + ", phone=" + phone + "]";
+  }
 }
--- Test.java
+++ Test.java
@@ -1,7 +1,26 @@
-public class Test{
-// 1)Prepare an Object of Emp() and Initialize a Variable
-// 2)Print Values using Getters()
-// 3)Prepare an Object of Emp() Initialize it through Args()Constructor
-// 4)Print Values by using toString();
-  
+public class Test {
+    public static void main(String[] args) {
+        // 1) Prepare an Object of Emp() and Initialize a Variable
+        Emp emp1 = new Emp(); 
+        emp1.setId(548); 
+        emp1.setName("vijay");
+        emp1.setSalary(50000.0);
+        emp1.setPhone(76749393L);
+
+        // 2) Print Values using Getters()
+        System.out.println("Employee Details using Getters:");
+        System.out.println("ID: " + emp1.getId());
+        System.out.println("Name: " + emp1.getName());
+        System.out.println("Salary: " + emp1.getSalary());
+        System.out.println("Phone: " + emp1.getPhone());
+        System.out.println();
+
+        
+        Emp emp2 = new Emp(666, "fulka", 60000.0, 9876543210L); 
+
+     
+        System.out.println("Employee Details using toString():");
+        System.out.println(emp2.toString());
+    }
 }
+
