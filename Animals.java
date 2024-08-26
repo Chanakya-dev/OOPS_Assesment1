4d56f22a543d95ba762ac7746d27f70dbd899aca
Vishal <87086820+Vishalprepro@users.noreply.github.com>
2024-08-26 17:35:17 +0530

    Update Animals.java

--- Animals.java
+++ Animals.java
@@ -1,4 +1,19 @@
-public interface Animals{
-// Prepare a Functoin called Eat() with String Paramater
-  
+interface Animals {
+    // Prepare a Functoin called Eat() with String Paramater
+    public String eat(String a, String b);
+}
+
+class tiger implements Animals {
+
+    public String eat(String a, String b) {
+        return a + b;
+    }
+}
+
+public class action {
+    public static void main(String[] args) {
+        tiger ts = new tiger();
+        String n = ts.eat("meat", "chicken");
+        System.out.println(n);
+    }
 }
