616796bc88a4e08d452778ab34e064d8930028ac
Aparna526 <aparnakondapaturu@gmail.com>
2024-08-26 19:06:32 +0530

    One

--- Emp.java
+++ Emp.java
@@ -12,16 +12,11 @@ public class Emp{
     return "Emp [Eid=" + Eid + ", Ename=" + Ename + ", EmobNo=" + EmobNo + ", Esalary=" + Esalary + "]";
   }
 
-  public static void main(String[] args) {
-    Emp E = new Emp();
-    Scanner sc = new Scanner(System.in);
-    E.Eid = sc.nextInt();
-    E.Ename = sc.next();
-    E.EmobNo = sc.nextLong();
-    E.Esalary = sc.nextDouble();
-
-    System.out.println(E.toString());
+  public Emp(int a, int b){
+    
   }
+
+ 
 
   
 }
--- Test.java
+++ Test.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
+
 public class Test{
 // 1)Prepare an Object of Emp() and Initialize a Variable
 // 2)Print Values using Getters()
 // 3)Prepare an Object of Emp() Initialize it through Args()Constructor
 // 4)Print Values by using toString();
+
+public static void main(String[] args) {
+    Emp E = new Emp();
+    Scanner sc = new Scanner(System.in);
+    E.Eid = sc.nextInt();
+    E.Ename = sc.next();
+    E.EmobNo = sc.nextLong();
+    E.Esalary = sc.nextDouble();    
+  }
   
 }
