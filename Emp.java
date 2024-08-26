e6f6bef626cc549a48fccfcc54ea751e05a6e0e4
Shoebmirza00 <shoebbaig31@gmail.com>
2024-08-26 19:09:39 +0530

    shoeb

--- Emp.java
+++ Emp.java
@@ -1,5 +1,27 @@
-public class Emp{
+public class Emp {
 // Take 4 Variables of Emp Data With an Access Modifier Private
   // Prepare toString()
+  private int age;
+  private String name;
+  private int num;
+  private int rank;
+  public Emp (int age , String name , int num , int i ){
+    this.age=age;
+    this.name=name;
+    this.num=num;
+    this.rank=i;
+
+  }
+  public Emp(){}
+  @Override
+  public String toString() {
+    return "Emp [age=" + age + ", name=" + name + ", num=" + num + ", rank=" + rank + "]";
+  }
+  
+  public static void main(String[] args) {
+    Emp li=new Emp(25,"mouni",30,1);
+    System.out.println(li.toString());
+
+  }
   
 }
